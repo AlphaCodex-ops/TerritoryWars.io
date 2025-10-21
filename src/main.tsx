57f14e258e1b50ed545618dce0ede882762c70d1
[dyad] <jjohan.anil@gmail.com>
2025-10-21 18:33:34 +0530

    [dyad] Adjusting react-dom import in main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
+import * as ReactDOMClient from "react-dom/client"; // Changed import style
 import App from "./App.tsx";
 import "./globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOMClient.createRoot(document.getElementById("root")!).render(<App />); // Using the namespace object
